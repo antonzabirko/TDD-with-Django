5ad592b4ddb676455875fb87018156175bc72bd9
Anton <antonzabirko@outlook.com>
2019-12-24 21:05:23 -0500

    First unit test and URL mapping; dummy view.

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,8 @@
+from django.urls import resolve
 from django.test import TestCase
+from lists.views import home_page
 
-class SmokeTest(TestCase):
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
