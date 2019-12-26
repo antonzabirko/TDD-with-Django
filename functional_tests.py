1708eccb3f1ff88b21dce2fcd72a81e0fe15d358
Anton <antonzabirko@outlook.com>
2019-12-25 23:22:53 -0500

    Refactored functional test duplicate code into a helper method.

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith goes to the homepage of a new to-do app.
         self.browser.get('http://localhost:8000')
@@ -32,9 +37,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy Peacock feathers" as an item on her to-do list.
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers.', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers.')
 
         # There is still a text box inviting her to enter another item.
         # She enters "Use Peacock feathers to make a fly".
@@ -44,11 +47,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # The page updates, and now shows both items on her to-do list.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly.',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers.')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly.')
 
         # Edith wonders if the site will remember her list. Then she sees that
         # the site has generated a random URL for her -- there is some explanatory
