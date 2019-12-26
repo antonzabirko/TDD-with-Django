cb956cba59c4acbf8feb21b6cc2755869377f3a6
Anton <antonzabirko@outlook.com>
2019-12-25 22:13:55 -0500

    Home page HTML now generated from a template.

--- functional_tests.py
+++ functional_tests.py
@@ -35,7 +35,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers.' for row in rows)
+            any(row.text == '1: Buy peacock feathers.' for row in rows),
+            "New to-do item did not appear in the list."
         )
 
         # There is still a text box inviting her to enter another item.
